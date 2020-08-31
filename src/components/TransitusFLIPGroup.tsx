094edbdcffadf57cad544e8f84ceaf7b5bb277e5
peoplesing1832 <peoplesing1832@gmail.com>
2020-08-31 10:56:47 +0800

    feat: 添加TransitusFLIPGroup组件

---
+++ src/components/TransitusFLIPGroup.tsx
@@ -0,0 +1,12 @@
+import React, {
+  useState,
+} from 'react';
+
+interface TransitusFLIPGroup {
+}
+
+const TransitusFLIPGroup: React.FC<TransitusFLIPGroup> = (props) => {
+  return null;
+}
+
+export default TransitusFLIPGroup;
