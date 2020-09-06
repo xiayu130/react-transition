cdeabd5265c6fce496ab629285d4d95ef687dfef
peoplesing1832 <peoplesing1832@gmail.com>
2020-09-06 16:13:01 +0800

    feat: 更新例子的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, {
-  useState,
+  useState, useEffect,
 } from 'react';
 import './App.css';
 import TransitusFLIP from './components/TransitusFLIP';
 import TransitusFLIPS from './components/TransitusFLIPS';
+import TransitusFLIPSQueue from './components/TransitusFLIPSQueue';
+import FLIP from './components/FLIP';
 
 const shuffle = function shuffle(array: any[]) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -32,72 +34,40 @@ function App() {
   }, {
     id: 6,
     text: 6
-  }, {
-    id: 7,
-    text: 7
-  }, {
-    id: 8,
-    text: 8
-  }, {
-    id: 9,
-    text: 9
-  }, {
-    id: 10,
-    text: 10
-  }, {
-    id: 11,
-    text: 11
-  }, {
-    id: 12,
-    text: 12
-  }, {
-    id: 13,
-    text: 13
-  }, {
-    id: 14,
-    text: 14
-  }, {
-    id: 15,
-    text: 15
-  }, {
-    id: 16,
-    text: 16
-  }, {
-    id: 17,
-    text: 17
-  }, {
-    id: 18,
-    text: 18
-  }, {
-    id: 19,
-    text: 19
-  }, {
-    id: 20,
-    text: 20
-  }, {
-    id: 21,
-    text: 21
   }]);
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   setInterval(() => {
+    //     document.getElementById('btn')?.click()
+    //   }, 30)
+    // }, 5000)
+  }, []);
 
 
   return (
     <div className="App">
-      <button onClick={() => {
-        console.log('点击')
+      <button id="btn" style={{
+        marginBottom: '30px'
+      }} onClick={() => {
         setList((prevlist) => {
           return [...shuffle(prevlist)];
         })
       }}>随机</button>
 
       <TransitusFLIPS
-        duration={800}
-        wrap="div"
-        wrapClassName="box"
+        duration={400}
       >
         {
           list && list.map(li => (
             <TransitusFLIP flipId={li.id} key={li.id}>
-              <div className="box-item">{ li.text }</div>
+              <div className="box-item"
+                onClick={() => {
+                  setList(prevList => {
+                    return prevList.filter(l => l.id !== li.id)
+                  })
+                }}
+              >{ li.text }</div>
             </TransitusFLIP>
           ))
         }
