c82b530cb65315f76444c7ec8a58d22996a856ee
zhangyue0219 <zhangyue0219@hupu.com>
2020-09-10 14:52:29 +0800

    fix: 入场动画完成后，强制更新一次缓存数据，避免flip的抖动

--- src/components/TransitionFLIP.tsx
+++ src/components/TransitionFLIP.tsx
@@ -141,6 +141,22 @@ const TransitionFLIP: React.FC<TransitionFLIP> = (props) => {
     <Transitus
       animation={animation}
       onLeave={onLeave}
+      onEnter={() => {
+        // 入场完成后强制更新一次缓存
+        const flipEle = selfRef.current;
+        if (flipEle) {
+          const parent = getParent(flipEle);
+          const parentRect = getRect(parent);
+          const rect = getRect(flipEle);
+          const styles = getStyles(flipEle);
+          rect.x = parentRect.x - rect.x;
+          rect.y = parentRect.y - rect.y;
+          catchStyles.set(FLIPID.current, {
+            rect,
+            styles,
+          });
+        }
+      }}
       duration={duration}
       transitionStyles={transitionStyles}
     >
