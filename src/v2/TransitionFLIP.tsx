309c9c143c59f4f213e839c95f41761d3487a4e7
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-11 08:16:17 +0800

    Update

---
+++ src/v2/TransitionFLIP.tsx
@@ -0,0 +1,180 @@
+import * as React from 'react';
+import {
+  useRef,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+} from 'react';
+import Transition from './Transition';
+import { FLIPSContext } from './TransitionFLIPS';
+import getRect from '../util/getReact';
+import getX from '../util/getX';
+import getY from '../util/getY';
+import getW from '../util/getW';
+import getH from '../util/getH';
+import getParent from '../util/getParent';
+import getStyles from '../util/getStyles';
+import createAnimation from '../util/createAnimation';
+
+export interface TransitionFLIP {
+  children: React.ReactElement;
+  flipId: string | number;
+  _onLeave?: () => void;
+  _animation?: boolean;
+  _name?: string;
+  _duration?: number;
+}
+
+const TransitionFLIP: React.FC<TransitionFLIP> = (props) => {
+
+  const {
+    _onLeave: onLeave,
+    _animation: animation,
+    _name: name,
+    _duration: duration,
+    flipId,
+    children,
+  } = props;
+
+  const {
+    catchStyles,
+    catchAnimations,
+    animationOption,
+  } = useContext(FLIPSContext);
+
+  const selfRef = useRef<HTMLElement>(null);
+  const firstMount = useRef<boolean>(true);
+  const FLIPID = useRef(flipId);
+
+  const force = () => {
+    const flipEle = selfRef.current;
+    const catchAnimation = catchAnimations.get(FLIPID.current);
+    if (
+      flipEle &&
+      catchAnimation
+    ) {
+      if (
+        catchAnimation.playState === 'running' &&
+        (catchAnimation as any)._isPlayed
+      ) {
+        const parent = getParent(flipEle);
+        const parentRect = getRect(parent);
+        // 基于父级进行计算
+        const rect = getRect(flipEle);
+        const styles = getStyles(flipEle);
+        rect.x = parentRect.x - rect.x;
+        rect.y = parentRect.y - rect.y;
+        catchStyles.set(FLIPID.current, {
+          rect,
+          styles,
+        });
+        (catchAnimation as any)._isPlayed = false;
+        catchAnimation.finish();
+      }
+    }
+  }
+
+  force();
+
+  useEffect(() => {
+    const flipEle = selfRef.current;
+    if (flipEle) {
+      // 由于getRect的强制更新屏幕，所以没有使用unmount的初始状态给
+      // Transition组件，也会有动画效果
+      const parent = getParent(flipEle);
+      const parentRect = getRect(parent);
+      const rect = getRect(flipEle);
+      const styles = getStyles(flipEle);
+      // 基于父级元素的
+      rect.x = parentRect.x - rect.x;
+      rect.y = parentRect.y - rect.y;
+      catchStyles.set(FLIPID.current, {
+        rect,
+        styles,
+      });
+    }
+  }, [catchStyles]);
+
+  useLayoutEffect(() => {
+    if (firstMount.current) {
+      firstMount.current = false;
+    } else {
+      const flipEle = selfRef.current
+      // 如果离场（比如删除元素）发生时，直接进入离开动画，flip动画直接过渡到最终状态
+      // Vue我看了官网的Demo也是如此处理的
+      if (flipEle) {
+        const catchRect = catchStyles.get(FLIPID.current);
+        const pbc = catchRect?.styles.backgroundColor;
+        const parent = getParent(flipEle);
+        const parentRect = getRect(parent);
+        const nextRect = getRect(flipEle);
+        const styles = getStyles(flipEle);
+        const cbc = styles.backgroundColor;
+        // 基于父级元素的
+        nextRect.x = parentRect.x - nextRect.x;
+        nextRect.y = parentRect.y - nextRect.y;
+        const x = getX(nextRect, catchRect?.rect as DOMRect);
+        const y = getY(nextRect, catchRect?.rect as DOMRect);
+        const w = getW(catchRect?.rect as DOMRect, nextRect);
+        const h = getH(catchRect?.rect as DOMRect, nextRect);
+        // 更新缓存
+        catchStyles.set(FLIPID.current, {
+          rect: nextRect,
+          styles,
+        });
+        if (x === 0 && y === 0 && w === 1 && h === 1 && pbc === cbc) {
+          return;
+        }
+        const animationKeyframes: Keyframe[] = [
+          {
+            transform: `translate(${x}px, ${y}px) scale(${w}, ${h})`,
+            backgroundColor: pbc,
+          },
+          {
+            transform: `translate(0, 0) scale(1, 1)`,
+            backgroundColor: cbc,
+          },
+        ];
+        const animationOptions: KeyframeAnimationOptions = animationOption;
+        const animation = createAnimation(flipEle, animationKeyframes, animationOptions);
+        catchAnimations.set(FLIPID.current, animation);
+        animation.play();
+        // _isPlayed 标示符，解决多次force的问题
+        (animation as any)._isPlayed = true;
+      }
+    }
+  });
+
+  const child = React.cloneElement(React.Children.only(children), {
+    ref: selfRef
+  });
+
+  return (
+    <Transition
+      animation={animation}
+      onLeaveed={onLeave}
+      onEntered={() => {
+        // 入场完成后强制更新一次缓存
+        const flipEle = selfRef.current;
+        if (flipEle) {
+          const parent = getParent(flipEle);
+          const parentRect = getRect(parent);
+          const rect = getRect(flipEle);
+          const styles = getStyles(flipEle);
+          rect.x = parentRect.x - rect.x;
+          rect.y = parentRect.y - rect.y;
+          catchStyles.set(FLIPID.current, {
+            rect,
+            styles,
+          });
+        }
+      }}
+      duration={duration}
+      name={name}
+    >
+      { child }
+    </Transition>
+  );
+};
+
+export default TransitionFLIP;
---
+++ src/v2/TransitionFLIPS.tsx
@@ -0,0 +1,228 @@
+import * as React from 'react';
+import {
+  useState,
+  useRef,
+  useEffect,
+} from 'react';
+import {
+  ChildrenMap
+} from './Observer';
+import {
+  TransitionFLIP
+} from './TransitionFLIP';
+
+type Rect = DOMRect | ClientRect;
+
+interface CatchStylesValue {
+  rect: Rect;
+  styles: CSSStyleDeclaration,
+}
+
+interface FLIPSContext {
+  catchStyles: CatchStylesMap;
+  catchAnimations: CatchAnimations;
+  animationOption: KeyframeAnimationOptions;
+}
+
+type CatchStylesMap = Map<string | number, CatchStylesValue>;
+type CatchAnimations = Map<string | number, Animation>;
+
+export const FLIPSContext = React.createContext<FLIPSContext>({
+  catchStyles: new Map(),
+  catchAnimations: new Map(),
+  animationOption: {},
+});
+
+interface TransitionFLIPS {
+  delay?: number;
+  duration?: number;
+  inOutDuration?: number;
+  easing?: string;
+  fill?: 'auto' | 'backwards' | 'both' | 'forwards' | 'none' | undefined;
+  wrap?: string;
+  wrapClassName?: string;
+  name?: string;
+}
+
+const TransitionFLIPS: React.FC<TransitionFLIPS> = (props) => {
+
+  const {
+    delay = 0,
+    duration = 200,
+    inOutDuration = 200,
+    easing = 'linear',
+    fill = 'auto',
+    wrap = 'div',
+    wrapClassName = '',
+    name = '',
+    children: _children,
+  } = props;
+
+  const handleLeave = (key: React.ReactText) => {
+    setChildren((prevChildren) => {
+      if (key in prevChildren) {
+        delete prevChildren[key];
+      }
+      return { ...prevChildren };
+    });
+  };
+
+  const mergeMap = (prev: ChildrenMap, next: ChildrenMap): ChildrenMap => {
+    prev = prev || {};
+    next = next || {};
+    function getValueForKey(key: React.ReactText) {
+      return key in next ? next[key] : prev[key];
+    }
+    let nextKeysPending = Object.create(null);
+    let pendingKeys = [];
+    for (let prevKey in prev) {
+      if (prevKey in next) {
+        if (pendingKeys.length) {
+          nextKeysPending[prevKey] = pendingKeys;
+          pendingKeys = [];
+        }
+      } else {
+        pendingKeys.push(prevKey);
+      }
+    }
+    let i;
+    let childMapping: ChildrenMap = {};
+    for (let nextKey in next) {
+      if (nextKeysPending[nextKey]) {
+        for (i = 0; i < nextKeysPending[nextKey].length; i++) {
+          let pendingNextKey = nextKeysPending[nextKey][i];
+          childMapping[pendingNextKey] = getValueForKey(
+            pendingNextKey
+          );
+        }
+      }
+      childMapping[nextKey] = getValueForKey(nextKey);
+    }
+    for (i = 0; i < pendingKeys.length; i++) {
+      childMapping[pendingKeys[i]] = getValueForKey(pendingKeys[i]);
+    }
+    return childMapping;
+  };
+
+  const getMap = (
+    children: React.ReactNode,
+    callback?: (child: React.ReactNode) => React.ReactNode
+  ): ChildrenMap => {
+    const map = Object.create(null);
+    if (children) {
+      // 如果没有手动添加key, React.Children.map会自动添加key
+      React.Children.map(children, c => c)?.forEach((child) => {
+        const key = (child as React.ReactElement).key || '';
+        if (key) {
+          if (React.isValidElement(child) && callback) {
+            map[key] = callback(child);
+          } else {
+            map[key] = child;
+          }
+        }
+      });
+    }
+    return map;
+  };
+
+  const initChildren = (
+    children: React.ReactNode,
+  ): ChildrenMap => {
+    return getMap(children, (child) => {
+      return React.cloneElement(child as React.ReactElement, {
+        _duration: inOutDuration,
+        _name: name,
+        _animation: true,
+        _onLeave: () => {
+          const key = (child as React.ReactElement).key || '';
+          handleLeave(key);
+        },
+      });
+    });
+  };
+
+  const nextChildren = (
+    nextChildren: React.ReactNode,
+    prevChildrenMap: ChildrenMap,
+  ): ChildrenMap => {
+    const nextChildrenMap = getMap(nextChildren);
+    const children = mergeMap(prevChildrenMap, nextChildrenMap);
+    Object.keys(children).forEach(key => {
+      const child = children[key];
+      if (!React.isValidElement(child)) {
+        return;
+      }
+      const hasKeyByNew = nextChildrenMap[key] !== undefined;
+      const hasKeyByPrev = prevChildrenMap[key] !== undefined;
+      const isNew = hasKeyByNew && !hasKeyByPrev;
+      const isDelete = !hasKeyByNew && hasKeyByPrev;
+      const isNeverChange = hasKeyByNew && hasKeyByPrev;
+      const prevProps = ((prevChildrenMap[key] as React.ReactElement)?.props as TransitionFLIP);
+      if (isNew) {
+        children[key] = React.cloneElement(child, {
+          _name: name,
+          _duration: inOutDuration,
+          _animation: true,
+          _onLeave: () => {
+            const key = (child as React.ReactElement).key || '';
+            handleLeave(key);
+          },
+        });
+      } else if (isDelete) {
+        children[key] = React.cloneElement(child, {
+          _animation: false,
+        });
+      } else if (isNeverChange) {
+        children[key] = React.cloneElement(child, {
+          _animation: prevProps._animation,
+          _duration: prevProps._duration,
+          _name: prevProps._name,
+          _onLeave: () => {
+            const key = (child as React.ReactElement).key || '';
+            handleLeave(key);
+          },
+        });
+      }
+    });
+    return children;
+  };
+
+  const firstMount = useRef<boolean>(true);
+  const [children, setChildren] = useState<ChildrenMap>(() => {
+    return initChildren(_children);
+  });
+  const catchStyles = useRef<CatchStylesMap>(new Map()).current;
+  const catchAnimations = useRef<CatchAnimations>(new Map()).current;
+  const animationOption: KeyframeAnimationOptions = {
+    delay,
+    duration,
+    easing,
+    fill,
+  };
+
+  useEffect(() => {
+    if (!firstMount.current) {
+      setChildren(nextChildren(_children, children));
+    } else {
+      firstMount.current = false;
+    }
+  }, [_children]);
+
+  const childNode = Object.values(children);
+
+  const wrapChildNode = React.createElement(wrap, {
+    className: wrapClassName,
+  }, childNode);
+
+  return (
+    <FLIPSContext.Provider value={{
+      catchStyles,
+      catchAnimations,
+      animationOption,
+    }}>
+      { wrapChildNode }
+    </FLIPSContext.Provider>
+  );
+}
+
+export default TransitionFLIPS;
