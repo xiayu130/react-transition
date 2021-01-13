a6165ded94df036efa2e55eec91a13007609fcde
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-13 15:28:01 +0800

    Update FLIP.tsx

--- src/v2/FLIP.tsx
+++ src/v2/FLIP.tsx
@@ -21,15 +21,14 @@ const FLIP: React.FC<FLIPProps> = (props) => {
     _animation: animation, // 离开，进入动画的开关
     _onLeaveed: onLeaveed,
     _name: name, // class的前缀
-    _duration: duration, // flip动画过渡的时长
-    _easing: easing, // flip动画的缓冲函数
   } = props as any;
 
   const selfRef = useRef<HTMLElement>();
   const prevRectRef = useRef<DOMRect>();
   const _reflowRef = useRef<number>();
+  const _runing = useRef<boolean>(false);
 
-  // 强制回流
+  // 强制重绘
   const reflow = () => {
     _reflowRef.current = document.body.offsetHeight;
   };
@@ -43,6 +42,34 @@ const FLIP: React.FC<FLIPProps> = (props) => {
     return rect;
   };
 
+  // 终止transition过渡，并保存当前的位置
+  const finish = () => {
+    const flipEle = selfRef.current;
+    if (flipEle) {
+      const s = flipEle.style;
+      const computedStyle = window.getComputedStyle(flipEle);
+      s.transitionDuration = '0s';
+      s.transform = s.webkitTransform = computedStyle.transform;
+    }
+  };
+
+  const addClass = (ele: HTMLElement, className: string): void => {
+    if (!className || !(className = className.trim())) {
+      return;
+    }
+    ele.classList.add(className);
+  };
+
+  const removeClass = (ele: HTMLElement, className: string): void => {
+    if (!className || !(className = className.trim())) {
+      return;
+    }
+    ele.classList.remove(className);
+    if (!ele.classList.length) {
+      ele.removeAttribute('class');
+    }
+  };
+
   const force = () => {
     const flipEle = selfRef.current;
     if (flipEle) {
@@ -50,12 +77,16 @@ const FLIP: React.FC<FLIPProps> = (props) => {
       const rect = relativeRect(parent, flipEle);
       prevRectRef.current = rect;
       // 这里可以增加一个停止css动画，并保存当前动画的位置（Vue这块没有做处理，可能并不重要）
+      if (_runing.current) {
+        // finish();
+      }
     }
   };
 
   useLayoutEffect(() => {
     const flipEle = selfRef.current
     if (flipEle && prevRectRef.current) {
+      const moveClass = `${name}-move`;
       const parent = getParent(flipEle);
       const nextRect = relativeRect(parent, flipEle);
       const prevRect = prevRectRef.current;
@@ -66,14 +97,14 @@ const FLIP: React.FC<FLIPProps> = (props) => {
         return;
       }
       const s = flipEle.style;
-      s.transitionDuration = '0s';
       s.transform = s.webkitTransform = `translate(${x}px,${y}px)`;
+      s.transitionDuration = '0s';
       // 强制重绘
       reflow();
-      // Vue实现，是添加了一个move类，move类包含了transition的内容
-      // 我在这里直接添加transition属性，并在transitionend事件时，删除transition属性
-      s.transition = `all ${duration}ms ${easing}`;
-      s.transform = s.webkitTransform = '';
+      // 添加move类
+      addClass(flipEle, moveClass);
+      s.transform = s.webkitTransform = s.transitionDuration = '';
+      _runing.current = true;
       // 监听transition事件
       flipEle.addEventListener('transitionend', function cb (e) {
         if (e && e.target !== flipEle) {
@@ -81,7 +112,9 @@ const FLIP: React.FC<FLIPProps> = (props) => {
         }
         if (!e || /transform$/.test(e.propertyName)) {
           flipEle.removeEventListener('transitionend', cb);
-          s.transition = '';
+          _runing.current = false;
+          // 删除move类
+          removeClass(flipEle, moveClass);
         }
       });
     }
